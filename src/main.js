8d4ed261a3e4daf32126bc00701ed37c15ee88bb
JamesSwatton <jamessswatton@gmail.com>
2020-08-25 15:32:32 +0100

    add tailwindcss

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-
 import { firestorePlugin } from "vuefire";
+import "./assets/tailwind.css";
 
 Vue.config.productionTip = false;
 
