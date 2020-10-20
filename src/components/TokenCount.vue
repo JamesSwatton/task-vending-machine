7d885076d2e57f77b2096cf8fa4dab24b9ac0ac4
JamesSwatton <jamessswatton@gmail.com>
2020-10-20 14:57:01 +0100

    add basic token update

--- src/components/TokenCount.vue
+++ src/components/TokenCount.vue
@@ -2,6 +2,7 @@
     <div class="px-8 pt-4">
         <h3 class="text-3xl leading-tight">Tokens</h3>
         <p class="text-3xl font-hairline leading-tight">{{ tokenCount }}</p>
+        <button @click="update">add</button>
     </div>
 </template>
 
@@ -19,7 +20,26 @@ export default {
     },
     computed: {
         tokenCount() {
-            return this.user.tokenCount;
+            return this.user.tokenTotal;
+        }
+    },
+    methods: {
+        update() {
+            var tokenCopy = { ...this.user }.tokenTotal;
+            const newTotal = (tokenCopy += 20);
+
+            console.log(tokenCopy);
+            db.collection("users")
+                .doc(firebase.auth().currentUser.uid)
+                .update({
+                    tokenTotal: newTotal
+                })
+                .then(() => {
+                    console.log("Token successfully updated!");
+                })
+                .catch(err => {
+                    console.error("Token update failed: " + err);
+                });
         }
     },
     created() {
