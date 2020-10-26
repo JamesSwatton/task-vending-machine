7bee8795500457ba5d47650ef42047daee4ba57c
JamesSwatton <jamessswatton@gmail.com>
2020-10-26 11:13:06 +0000

    add button png to vending button

--- src/components/vending/Keypad.vue
+++ src/components/vending/Keypad.vue
@@ -1,77 +1,137 @@
 <template>
     <div>
-        <h3 class="w-32 mx-auto text-3xl">{{ displayNum }}</h3>
-        <div class="w-32 mx-auto grid grid-cols-3">
+        <h3 id="display" class="w-32 mx-auto text-3xl">{{ displayNum }}</h3>
+        <div class="w-32 mx-auto grid grid-cols-3 gap-1">
             <button
-                class="keypad-button hover:bg-gray-200 active:bg-gray-500"
+                class="keypad-button hover:bg-gray-200 active:bg-gray-500 "
                 @click="buttonPress(7)"
             >
+                <img
+                    class="absolute top-0 left-0"
+                    src="../../assets/vending_button.png"
+                    alt="vending button"
+                />
                 7
             </button>
             <button
                 class="keypad-button hover:bg-gray-200 active:bg-gray-500"
                 @click="buttonPress(8)"
             >
+                <img
+                    class="absolute top-0 left-0"
+                    src="../../assets/vending_button.png"
+                    alt="vending button"
+                />
                 8
             </button>
             <button
                 class="keypad-button hover:bg-gray-200 active:bg-gray-500"
                 @click="buttonPress(9)"
             >
+                <img
+                    class="absolute top-0 left-0"
+                    src="../../assets/vending_button.png"
+                    alt="vending button"
+                />
                 9
             </button>
             <button
                 class="keypad-button hover:bg-gray-200 active:bg-gray-500"
                 @click="buttonPress(4)"
             >
+                <img
+                    class="absolute top-0 left-0"
+                    src="../../assets/vending_button.png"
+                    alt="vending button"
+                />
                 4
             </button>
             <button
                 class="keypad-button hover:bg-gray-200 active:bg-gray-500"
                 @click="buttonPress(5)"
             >
+                <img
+                    class="absolute top-0 left-0"
+                    src="../../assets/vending_button.png"
+                    alt="vending button"
+                />
                 5
             </button>
             <button
                 class="keypad-button hover:bg-gray-200 active:bg-gray-500"
                 @click="buttonPress(6)"
             >
+                <img
+                    class="absolute top-0 left-0"
+                    src="../../assets/vending_button.png"
+                    alt="vending button"
+                />
                 6
             </button>
             <button
                 class="keypad-button hover:bg-gray-200 active:bg-gray-500"
                 @click="buttonPress(1)"
             >
+                <img
+                    class="absolute top-0 left-0"
+                    src="../../assets/vending_button.png"
+                    alt="vending button"
+                />
                 1
             </button>
             <button
                 class="keypad-button hover:bg-gray-200 active:bg-gray-500"
                 @click="buttonPress(2)"
             >
+                <img
+                    class="absolute top-0 left-0"
+                    src="../../assets/vending_button.png"
+                    alt="vending button"
+                />
                 2
             </button>
             <button
                 class="keypad-button hover:bg-gray-200 active:bg-gray-500"
                 @click="buttonPress(3)"
             >
+                <img
+                    class="absolute top-0 left-0"
+                    src="../../assets/vending_button.png"
+                    alt="vending button"
+                />
                 3
             </button>
             <button
                 class="keypad-button hover:bg-gray-200 active:bg-gray-500"
                 @click="clear()"
             >
+                <img
+                    class="absolute top-0 left-0"
+                    src="../../assets/vending_button.png"
+                    alt="vending button"
+                />
                 *
             </button>
             <button
                 class="keypad-button hover:bg-gray-200 active:bg-gray-500"
                 @click="buttonPress(0)"
             >
+                <img
+                    class="absolute top-0 left-0"
+                    src="../../assets/vending_button.png"
+                    alt="vending button"
+                />
                 0
             </button>
             <button
                 class="keypad-button hover:bg-gray-200 active:bg-gray-500"
                 @click="enter"
             >
+                <img
+                    class="absolute top-0 left-0"
+                    src="../../assets/vending_button.png"
+                    alt="vending button"
+                />
                 #
             </button>
         </div>
@@ -88,13 +148,19 @@ export default {
     },
     methods: {
         buttonPress(num) {
-            if (this.inputNum.length == 1 && num == 0) {
-                this.inputNum.push(num);
-            } else if (this.inputNum.length == 1 && this.inputNum[0] == "0" && num !== 0) {
-                this.inputNum.pop()
-                this.inputNum.push(num)
-            } else {
-                this.inputNum.push(num)
+            if (this.inputNum.length < 7) {
+                if (this.inputNum.length == 1 && num == 0) {
+                    this.inputNum.push(num);
+                } else if (
+                    this.inputNum.length == 1 &&
+                    this.inputNum[0] == "0" &&
+                    num !== 0
+                ) {
+                    this.inputNum.pop();
+                    this.inputNum.push(num);
+                } else {
+                    this.inputNum.push(num);
+                }
             }
         },
         clear() {
@@ -116,6 +182,6 @@ export default {
 
 <style>
 .keypad-button {
-    @apply w-10 h-10 border;
+    @apply w-10 h-10 relative;
 }
 </style>
